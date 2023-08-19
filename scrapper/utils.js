06a2c5213412677dfc29ba84f756005de11a4686
LokeshPatil-loki <lokesh1404@outlook.com>
2023-08-19 21:47:26 +0530

    added utility function to get image of company profile

---
+++ scrapper/utils.js
@@ -0,0 +1,11 @@
+export function getImageUrlByEntityUrn(json,entityUrn){
+    const companyImageEntity = json?.included.find(object => object.entityUrn == entityUrn);
+    const vectorImage = companyImageEntity?.logoResolutionResult?.vectorImage;
+    const rootUrl = vectorImage?.rootUrl;
+    const fileIdentifyingUrlPathSegment = vectorImage?.artifacts[0].fileIdentifyingUrlPathSegment;
+    if(rootUrl && fileIdentifyingUrlPathSegment){
+        return rootUrl+fileIdentifyingUrlPathSegment;
+    }else{
+        return undefined;
+    }
+}
