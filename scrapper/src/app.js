cde1acb68611232104f754016eafd3f9f4ac2f24
Scrap-Linkedin <scrape-linkedin@outlook.com>
2023-08-27 04:14:28 +0530

    Moved Express app to app.js

--- scrapper/src/app.js
+++ scrapper/src/app.js
@@ -1,40 +1,32 @@
+import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { getUserHandle } from "./utils.js";
+import { NotFoundError } from "./errors/NotFoundError.js";
+import { BadRequestError } from "./errors/BadRequestError.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { CustomError } from "./errors/CustomError.js";
+import { profileRoutes } from "./routes/profileRoutes.js";
+
 dotenv.config();
-import fs from "fs";
 
-import {getProfileTopCard} from "./profile/ProfileTopCard.js";
-import {getMidCard} from "./profile/midCard.js";
-import {getExpriences} from "./profile/experiences.js"
-import { getEducation } from "./profile/education.js";
-import { getCertifications } from "./profile/certifications.js";
-import {getSkills} from "./profile/skills.js";
-import {getPosts} from "./profile/posts.js";
+const PORT = 4000;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.use(profileRoutes);
 
-export async function getProfile(userHandle){
-  try {
-    const profileTopCard = await getProfileTopCard(userHandle);
-    const about = await getMidCard(userHandle, profileTopCard.authorProfileId);
-    const experiences = await getExpriences(userHandle, profileTopCard.authorProfileId);
-    const education = await getEducation(userHandle, profileTopCard.authorProfileId);
-    const certifications = await getCertifications(userHandle, profileTopCard.authorProfileId);
-    const skills = await getSkills(userHandle, profileTopCard.authorProfileId);
-    const posts = await getPosts(userHandle,profileTopCard.authorProfileId);
-    const profile = {
-      ...profileTopCard,
-      about,
-      experiences,
-      education,
-      certifications,
-      skills,
-      posts
-    };
-    return profile;
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
-}
+app.all("*", (req, res,next) => {
+    try {
+        throw new NotFoundError();
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.use(errorHandler);
 
 
 
+export default app;
