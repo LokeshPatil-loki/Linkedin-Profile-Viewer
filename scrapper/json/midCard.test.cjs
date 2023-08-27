0d06339451ffdeb02e6ac306f65965d18111f1a0
LokeshPatil-loki <lokesh1404@outlook.com>
2023-08-27 05:50:35 +0530

    Cleanup

--- scrapper/json/midCard.test.cjs
+++
@@ -1,7 +0,0 @@
-const midCard = require("./midCard.json");
-
-// const about = midCard?.included[5].topComponents[1].components.textComponent.text.text;
-// console.log(about);
-const component = midCard?.included?.find(item => item.entityUrn?.includes("ABOUT"));
-
-console.log(component?.topComponents?.[1].components?.textComponent?.text?.text);
